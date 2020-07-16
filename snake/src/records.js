2bc070144b2aeeafef2af35dd0e53575433e5e3a
kirillgrishin-tech <55716229+kirillgrishin-tech@users.noreply.github.com>
2020-07-16 22:33:02 +0300

    fix

--- snake/src/records.js
+++ snake/src/records.js
@@ -34,14 +34,6 @@ function Records({prep,add,setAdd,setStyle,color}){
     <div className='GameP' style={{color: color}}>
         <h1 style={{marginBottom: '1em'}}>Список рекордов</h1>
         {rec ? <Table color={color} add={add} rec={rec} setValue={setValue} value={value}/>:<Loading color={color}/>}
-
-            {add !=0 && <tr>
-              <td style={{width: '10%'}}></td>
-              <td style={{width:'20%'}}></td>
-              <td style={{border: `2px solid ${color}`}}>Введите имя: <input value={value} onChange={(event) => {setValue(event.target.value)}}/></td>
-              <td style={{border: `2px solid ${color}`,width: '10%'}}>{add}</td>
-            </tr>
-            }
         {add === 0 ? 
             <button className = 'btn' style={{margin: '1em', color: 'blue'}} onClick={()=>{setStyle('fixed'); prep('init');}}>Назад</button>:<button className = 'btn' style={{margin: '1em', color: 'blue'}} onClick={()=>{addRecord(value,add); setAdd(0);}}>Добавить</button>}
     </div>
