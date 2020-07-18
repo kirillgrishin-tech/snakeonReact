ccc54894ef9a4441662a7a3bf97512525462501c
kirillgrishin-tech <55716229+kirillgrishin-tech@users.noreply.github.com>
2020-07-18 13:05:12 +0300

    Fix

--- snake/src/preparePane.js
+++ snake/src/preparePane.js
@@ -28,120 +28,78 @@ function PrepareGamePane ({level,field,cell,makeFood,initsnake,comparOGO}){
             let headCell = head.map(function(value, index){ return value + direction[direct][index] });
             this.body = comparOGO(headCell,body);
         }}
-    document.addEventListener('keydown', keyHandler, false);
+    document.addEventListener('keydown', keyHandler, false);      
 
-    function remswipefc(){
-        let     swipedir,
-                startX,
-                startY,
-                distX,
-                distY,
-                threshold = 80,
-                restraint = 100,
-                allowedTime = 5000,
-                elapsedTime,
-                startTime
-    
-        document.removeEventListener('touchstart', function(e){
-            let touchobj = e.changedTouches[0]
-            swipedir = 'none'
-            startX = touchobj.pageX
-            startY = touchobj.pageY
-            startTime = new Date().getTime() // record time when finger first makes contact with surface
-        })
-    
-        document.removeEventListener('touchmove', function(e){
-            e.preventDefault() // prevent scrolling when inside DIV
-        })
-    
-        document.removeEventListener('touchend', function(e){
-            let touchobj = e.changedTouches[0]
-            distX = touchobj.pageX - startX 
-            distY = touchobj.pageY - startY 
-            elapsedTime = new Date().getTime() - startTime 
-            if (elapsedTime <= allowedTime){ 
-                if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ 
-                    swipedir = (distX < 0)? 'left' : 'right'
-                }
-                else if (Math.abs(distY) >= threshold  && Math.abs(distX) <= restraint){ 
-                    swipedir = (distY < 0)? 'up' : 'down'
-                }
-            }
-            handleswipe(swipedir)
-        })
+function swipefc(){
+let     swipedir,
+        startX,
+        startY,
+        distX,
+        distY,
+        threshold = 80,
+        restraint = 100,
+        allowedTime = 5000,
+        elapsedTime,
+        startTime
+
+document.addEventListener('touchstart', function(e){
+    let touchobj = e.changedTouches[0]
+    swipedir = 'none'
+    startX = touchobj.pageX
+    startY = touchobj.pageY
+    startTime = new Date().getTime() // record time when finger first makes contact with surface
+}, false)
+
+document.addEventListener('touchmove', function(e){
+    e.preventDefault() // prevent scrolling when inside DIV
+}, false)
+
+document.addEventListener('touchend', function(e){
+    let touchobj = e.changedTouches[0]
+    distX = touchobj.pageX - startX 
+    distY = touchobj.pageY - startY 
+    elapsedTime = new Date().getTime() - startTime 
+    if (elapsedTime <= allowedTime){ 
+        if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ 
+            swipedir = (distX < 0)? 'left' : 'right'
+        }
+        else if (Math.abs(distY) >= threshold  && Math.abs(distX) <= restraint){ 
+            swipedir = (distY < 0)? 'up' : 'down'
+        }
     }
-        
+    handleswipe(swipedir)
+}, false)
+}
 
-            function swipefc(){
-                let     swipedir,
-                        startX,
-                        startY,
-                        distX,
-                        distY,
-                        threshold = 80,
-                        restraint = 100,
-                        allowedTime = 5000,
-                        elapsedTime,
-                        startTime
-            
-                document.addEventListener('touchstart', function(e){
-                    let touchobj = e.changedTouches[0]
-                    swipedir = 'none'
-                    startX = touchobj.pageX
-                    startY = touchobj.pageY
-                    startTime = new Date().getTime() // record time when finger first makes contact with surface
-                }, true)
-            
-                document.addEventListener('touchmove', function(e){
-                    e.preventDefault() // prevent scrolling when inside DIV
-                }, false)
-            
-                document.addEventListener('touchend', function(e){
-                    let touchobj = e.changedTouches[0]
-                    distX = touchobj.pageX - startX 
-                    distY = touchobj.pageY - startY 
-                    elapsedTime = new Date().getTime() - startTime 
-                    if (elapsedTime <= allowedTime){ 
-                        if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ 
-                            swipedir = (distX < 0)? 'left' : 'right'
-                        }
-                        else if (Math.abs(distY) >= threshold  && Math.abs(distX) <= restraint){ 
-                            swipedir = (distY < 0)? 'up' : 'down'
-                        }
-                    }
-                    handleswipe(swipedir)
-                }, false)
-            }
-                
-                    function handleswipe(swipe){
-                        switch (swipe){
-                            case 'left':
-                                if (direct !== 0){
-                                    directx = 2;
-                                }
-                                break;
-            
-                            case 'right':
-                                if (direct !== 2){
-                                    directx = 0;
-                                }
-                                break;
-            
-                            case 'up':
-                                if (direct !== 1){
-                                    directx = 3;
-                                }
-                                break;
-            
-                            case 'down':
-                                if (direct !== 3){
-                                    directx = 1;
-                                }
-                                break;
-                            default:
-                                break;
-                        }
-                    }
+    function handleswipe(swipe){
+        switch (swipe){
+            case 'left':
+                if (direct !== 0){
+                    directx = 2;
+                }
+                break;
+
+            case 'right':
+                if (direct !== 2){
+                    directx = 0;
+                }
+                break;
+
+            case 'up':
+                if (direct !== 1){
+                    directx = 3;
+                }
+                break;
+
+            case 'down':
+                if (direct !== 3){
+                    directx = 1;
+                }
+                break;
+            default:
+                break;
+        }
+    }
 
     function keyHandler (event){
         switch (event.keyCode) {
@@ -202,7 +160,6 @@ function PrepareGamePane ({level,field,cell,makeFood,initsnake,comparOGO}){
         let inter = setInterval(() => {snake.move();},level);
         return ()=>{
             clearInterval(inter);
-            remswipefc();
         }
         // eslint-disable-next-line
     },[level])
